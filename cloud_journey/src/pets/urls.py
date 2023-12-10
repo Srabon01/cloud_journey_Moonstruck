becb06744b89706f6bd259b1664cfaeaf07a6808
srabon01 <117457648+Srabon01@users.noreply.github.com>
2023-12-10 15:07:04 +0600

    create view

--- cloud_journey/src/pets/urls.py
+++ cloud_journey/src/pets/urls.py
@@ -1,7 +1,9 @@
 #cloud_journey/src/pets/urls.py
 from django.urls import path
-from pets.views import PetsListView
+from pets.views import PetsListView, PetCreateView
 
 urlpatterns = [
     path("all/", PetsListView.as_view(), name="pet-list"),
+    path("add/", PetCreateView.as_view(), name="add-pet"),
+    
 ]
